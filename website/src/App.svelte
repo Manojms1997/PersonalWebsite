f72d6b1bda060c5dce94800bc59503826426d9ec
ManojMS <manojsrinath97@gmail.com>
2021-12-17 20:47:11 -0500

    Added Header component

--- website/src/App.svelte
+++ website/src/App.svelte
@@ -1,51 +1,10 @@
 <script>
-  import { HtmlTag } from "svelte/internal";
-
-  let isThemeDark = true;
-  const html = document.querySelector("html");
-
-  var changeTheme = () => {
-    console.log(isThemeDark);
-    if (isThemeDark) {
-      html.classList.remove("dark");
-    } else {
-      html.classList.add("dark");
-    }
-    isThemeDark = !isThemeDark;
-  };
+  import Header from './Header.svelte';
+ 
 </script>
 
 <div class="first-pane">
-  <div class="header">
-    <div class="header-left">
-      <img src="favicon.png" alt="Manoj" width="50px" height="50px" />
-    </div>
-    <div class="header-right">
-      <a
-        class="header-items text-white hover:font-semibold hover:text-yellow-900"
-        href="">About</a
-      >
-      <a class="header-items text-white" href="">Projects</a>
-      <a class="header-items text-white" href="">Contact</a>
-      <div on:click={changeTheme}>
-        {#if isThemeDark}
-          <img
-            src="theme-change-dark.svg"
-            alt="theme-change"
-            id="theme-change-dark"
-            class="header-items"
-          />
-        {:else}
-          <img
-            src="theme-change-light.svg"
-            alt="theme-change"
-            id="theme-change-light"
-            class="header-items"
-          />
-        {/if}
-      </div>
-    </div>
-  </div>
+  <Header/>
   <div class="first-pane-body">
     <h1
       class="text-7xl font-sans text-stone-900 hover:text-yellow-900 dark:text-white"
@@ -66,16 +25,6 @@
     width: 100vw;
   }
 
-  .header-items {
-    margin: 2vw;
-  }
-  /* #theme-change-icon {
-	  margin-top: 2vh;
-	  margin-bottom: 2vh;
-	  margin-left: 2vw;
-	  margin-right: 2vw;
-  } */
-
   .first-pane-body {
     height: 90vh;
     display: flex;
@@ -84,28 +33,7 @@
     align-items: center;
   }
 
-  .header {
-    height: 10vh;
-    width: 100vw;
-    display: flex;
-    justify-content: flex-end;
-    align-items: center;
-  }
-  .header-left {
-    height: 10vh;
-    width: 30vw;
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
-  }
 
-  .header-right {
-    height: 10vh;
-    width: 70vw;
-    display: flex;
-    justify-content: flex-end;
-    align-items: center;
-  }
   .h1 {
     font-size: 7vh;
     font-family: "Brush Script MT", cursive;
---
+++ website/src/Header.svelte
@@ -0,0 +1,71 @@
+<script>
+  let isThemeDark = true;
+  const html = document.querySelector("html");
+
+  var changeTheme = () => {
+    console.log(isThemeDark);
+    if (isThemeDark) {
+      html.classList.remove("dark");
+    } else {
+      html.classList.add("dark");
+    }
+    isThemeDark = !isThemeDark;
+  };
+</script>
+
+<div class="header">
+  <div class="header-left">
+    <img src="favicon.png" alt="Manoj" width="50px" height="50px" />
+  </div>
+  <div class="header-right">
+    <div class="header-items text-stone-900 dark:text-white">About</div>
+    <div class="header-items text-stone-900 dark:text-white">Projects</div>
+    <div class="header-items text-stone-900 dark:text-white">Contact</div>
+    <div on:click={changeTheme} width="20px">
+      {#if isThemeDark}
+        <img
+          src="theme-change-dark.svg"
+          alt="theme-change"
+          id="theme-change-dark"
+          class="header-items"
+        />
+      {:else}
+        <img
+          src="theme-change-light.svg"
+          alt="theme-change"
+          id="theme-change-light"
+          class="header-items"
+        />
+      {/if}
+    </div>
+  </div>
+</div>
+
+<style>
+  .header-items {
+    margin: 2vw;
+  }
+
+  .header {
+    height: 10vh;
+    width: 100vw;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+  }
+  .header-left {
+    height: 10vh;
+    width: 30vw;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+  }
+
+  .header-right {
+    height: 10vh;
+    width: 70vw;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+  }
+</style>
