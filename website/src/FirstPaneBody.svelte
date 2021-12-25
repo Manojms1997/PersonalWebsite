559daf70ab916b98c55abdf6e9bb9e2cafabd1c5
ManojMS <manojsrinath97@gmail.com>
2021-12-25 00:19:18 -0500

    First pane style changed

--- website/src/FirstPaneBody.svelte
+++ website/src/FirstPaneBody.svelte
@@ -1,8 +1,8 @@
-<div class="first-pane-body ml-28">
-    <h1 class="text-7xl font-sans text-stone-900 dark:text-white">
-        <div class="inline-display text-8xl">H</div>i!<br /> <div class="inline-display text-8xl">I</div>'m <div class="inline-display text-8xl">M</div>ANOJ 
-      <div class="inline-display text-8xl">M</div>YSORE <div class="inline-display text-8xl">S</div>RINATH<br /> 
-      <div class="inline-display text-8xl">I</div>'m a <div class="inline-display text-8xl">S</div>oftware <div class="inline-display text-8xl">E</div>ngineer
+<div class="first-pane-body">
+    <h1 class="text-5xl lg:text-7xl font-sans text-stone-900 dark:text-white">
+        <div class="inline-display text-6xl lg:text-8xl">H</div>i!<br /> <div class="inline-display text-6xl lg:text-8xl">I</div>'m <div class="inline-display text-6xl lg:text-8xl">M</div>ANOJ 
+      <div class="inline-display text-6xl lg:text-8xl">M</div>YSORE <div class="inline-display text-6xl lg:text-8xl">S</div>RINATH<br /> 
+      <div class="inline-display text-6xl lg:text-8xl">I</div>'m a <div class="inline-display text-6xl lg:text-8xl">S</div>oftware <div class="inline-display text-6xl lg:text-8xl">E</div>ngineer
     </h1>
     <!-- <img
       src="down-icon-light.svg"
